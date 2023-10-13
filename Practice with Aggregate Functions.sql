df8c3ba654ed2eb0c809e1a8ca07e2cdb761408d
Charmaine Vinarta <120625052+CharmaineVinarta@users.noreply.github.com>
2023-10-12 17:11:36 -0700

    Create Practice with Aggregate Functions.sql

---
+++ Practice with Aggregate Functions.sql
@@ -0,0 +1,19 @@
+-- Use the SUM() function to calculate the total duration of all films and alias with total_duration.
+-- Query the sum of film durations
+SELECT SUM(duration) AS total_duration
+FROM films;
+
+-- Calculate the average duration of all films and alias with average_duration.
+-- Calculate the average duration of all films
+SELECT AVG(duration) AS average_duration
+FROM films;
+
+-- Find the most recent release_year in the films table, aliasing as latest_year.
+-- Find the latest release_year
+SELECT MAX(release_year) AS latest_year
+FROM films;
+
+-- Find the duration of the shortest film and use the alias shortest_film.
+-- Find the duration of the shortest film
+SELECT MIN(duration) AS shortest_film
+FROM films;
